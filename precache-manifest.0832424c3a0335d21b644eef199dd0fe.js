a1e99cbfc947604ad9c39fe51aefd0548a360a83
Deepak Dhaker <ddhaker2000@gmail.com>
2023-07-27 17:35:33 +0530

    Updates

---
+++ precache-manifest.0832424c3a0335d21b644eef199dd0fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81b304a5aec75f92a177",
+    "url": "/emoji-browser/static/css/main.92d8cc28.chunk.css"
+  },
+  {
+    "revision": "81b304a5aec75f92a177",
+    "url": "/emoji-browser/static/js/main.81b304a5.chunk.js"
+  },
+  {
+    "revision": "537741e50dfa0f444ed8",
+    "url": "/emoji-browser/static/js/1.537741e5.chunk.js"
+  },
+  {
+    "revision": "cd61e18fa1efc469bf11",
+    "url": "/emoji-browser/static/js/runtime~main.cd61e18f.js"
+  },
+  {
+    "revision": "1a9ee33f3c804c35d85cb00053141ef6",
+    "url": "/emoji-browser/index.html"
+  }
+];
